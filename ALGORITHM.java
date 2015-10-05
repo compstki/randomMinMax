3af71d1ba8d31d1a011c4e87527a4c0d1d7adf91
comptski <brendan.mccart@stkentigernsacademy.org.uk>
2015-10-05 17:09:11 +0100

    randomMinMax v1
    version 1

---
+++ ALGORITHM.java
@@ -0,0 +1,42 @@
+
+public class ALGORITHM  {
+    // declare an array which can store DATA objects
+    private DATA[] dataList;
+
+    // constructor
+    public ALGORITHM()  {
+        // set the size of the array
+        dataList = new DATA[20];
+    }
+
+    // populate the array with DATA objects
+    public void setUpDataList()  {
+        // loop for each item in the array
+        for (int i=0; i<20; i++) {
+            //create a new DATA object and store in next array position
+            dataList[i] = new DATA();
+        }
+    }
+
+    // standard algorithm to find largest value, tracking POSITION of value
+    public void findMinData()  {
+        // choose position of first value
+        int minDataPosition = 0;
+
+        // repeat for the REST of the array
+        for (int i=1; i<20; i++) {
+
+            //compare current value with best value
+            if (dataList[i].getData() < dataList[minDataPosition].getData()) {
+                // update the position of the best value
+                minDataPosition = i;
+            }
+        }
+
+        //display results: position and the best (max) value
+        System.out.print("Position is:" + minDataPosition + " , Value is:");
+        dataList[minDataPosition].displayData();
+        System.out.println();
+    }
+
+}
---
+++ DATA.java
@@ -0,0 +1,40 @@
+// include add-on module libraries
+import java.util.Random;
+import javax.swing.JOptionPane;    
+
+public class DATA {
+    // randomGenerator can "make" random numbers
+    private Random randomGenerator = new Random();
+    // random number will store an actual random number made by the randomGenerator
+    private int randomNumber;
+
+    public DATA() {
+        // two alternative ways to set up a value, random number or keybaord entry
+        // makeData();
+        // enterData();
+    }
+
+    public void makeData()    {
+        // make a new random number and store it
+        randomNumber = randomGenerator.nextInt(100);
+    }
+
+    public void enterData() {
+        // use inpupt validation to enter a value 0-100
+        randomNumber = Integer.parseInt(JOptionPane.showInputDialog("Enter a number ;"));
+        while ((randomNumber < 0) || (randomNumber > 100)) {
+            randomNumber = Integer.parseInt(JOptionPane.showInputDialog("Error, enter a number ;"));
+        }
+    }
+
+    // allow another object to ask for the current value
+    public int getData()    {
+        return randomNumber;
+    }
+
+    // display current value on screen
+    public void displayData()    {
+        System.out.print(randomNumber);
+    }
+
+}
